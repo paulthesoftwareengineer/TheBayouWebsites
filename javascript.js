0264fa7f07c5fd2d9804c660714833dcdfb3aacc
Paul Turner <102317855+paulthesoftwareengineer@users.noreply.github.com>
2025-04-06 14:25:49 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    navToggle.addEventListener('click', () => {
+        const isExpanded = navToggle.getAttribute('aria-expanded') === 'true';
+        navToggle.setAttribute('aria-expanded', !isExpanded);
+        navMenu.classList.toggle('active');
+    });
+});
