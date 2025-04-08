728c281329a443f98b11fd75d6455a6275f01835
Paul <paulthesoftwareengineer@outlook.com>
2025-04-07 21:33:43 -0700

    updated all

--- javascript.js
+++ javascript.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    navToggle.addEventListener('click', () => {
+        const isExpanded = navToggle.getAttribute('aria-expanded') === 'true';
+        navToggle.setAttribute('aria-expanded', !isExpanded);
+        navMenu.classList.toggle('open');
+    });
+});
